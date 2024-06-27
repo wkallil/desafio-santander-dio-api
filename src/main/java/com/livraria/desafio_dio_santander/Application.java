818f3b67dd978dfa2ee09d42310c1cffab087df2
Wkallil <kallil.tim@gmail.com>
2024-06-27 13:56:55 -0300

    Dio Desafio Santander API RESTful  Railway

---
+++ src/main/java/com/livraria/desafio_dio_santander/Application.java
@@ -0,0 +1,13 @@
+package com.livraria.desafio_dio_santander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
